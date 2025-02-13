90dad2ed021ba4faf2bde623a3a1dca54b2a9aaa
petersafwat22 <petersafwat645@gmail.com>
2025-02-13 10:33:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(
     origin: ["http://localhost:3000", "https://xtrack-frontend.vercel.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    // credentials: true,
     optionsSuccessStatus: 200,
   })
 );
